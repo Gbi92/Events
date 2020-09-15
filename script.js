d3b6592d909d78e105cc10d66d044b6899faf9bb
Gbi92 <70910864+Gbi92@users.noreply.github.com>
2020-09-15 13:01:59 +0200

    tried out click event

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ $('#btn2').on ('click', () => {
     $('#btn1').text("Why you do this?");
 });
 
-$('#btn3').on ('click', () => {
-    $('button').css("background-color", "green");
+// $('#btn3').on ('click', () => {
+//     $('button').css("background-color", "coral");
+// });
+
+$('#btn3').one ('click', () => {
+    $('button').css("background-color", $('input').val());
 });
